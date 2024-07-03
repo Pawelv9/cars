8747d007a949da0f4159a073541d1361e16fc776
Pavel <pawelv9@gmail.com>
2024-07-03 14:28:20 +0300

    listing the records

--- src/components/CarList.js
+++ src/components/CarList.js
@@ -6,8 +6,30 @@ function CarList(props) {
         return state.cars.data
     })
 
+    const handleCarDelete = car => {
+
+    }
+
+    const renderedCars = cars.map((car) => {
+        return (
+            <div key={car.id} className="panel">
+                <p>
+                    {car.name} -${car.cost}
+                </p>
+                <button className='button is-danger'
+                onClick={() => handleCarDelete(car)}
+                >
+                    Delete
+                </button>
+            </div>
+        )
+    })
+
     return (
-        <div>CarList</div>
+        <div className='car-list'>
+            {renderedCars}
+            <hr />
+        </div>
     );
 }
 
