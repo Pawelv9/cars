3051a7e6bf485f06250216890d9a652010e6015d
Pavel <pawelv9@gmail.com>
2024-07-03 12:57:02 +0300

    receiving the Cost

--- src/components/CarForm.js
+++ src/components/CarForm.js
@@ -1,28 +1,44 @@
-import React from 'react';
-import {useDispatch, useSelector} from "react-redux";
-import {changeName} from "../store";
+import { useDispatch, useSelector } from 'react-redux';
+import { changeName, changeCost } from '../store';
 
-function CarForm(props) {
+function CarForm() {
     const dispatch = useDispatch();
-    const name = useSelector(state => {
-        return state.form.name
-    })
+    const { name, cost } = useSelector((state) => {
+        return {
+            name: state.form.name,
+            cost: state.form.cost,
+        };
+    });
 
-    const handleNameChange = event => {
+    const handleNameChange = (event) => {
         dispatch(changeName(event.target.value));
-    }
+    };
+    const handleCostChange = (event) => {
+        const carCost = parseInt(event.target.value) || 0;
+        dispatch(changeCost(carCost));
+    };
 
     return (
-        <div className="car-form panel">
+        <div className='car-form panel'>
             <h4 className='subtitle is-3'>Add Car</h4>
             <form>
-                <div className="field-group">
-                    <div className="field">
+                <div className='field-group'>
+                    <div className='field'>
                         <label className='label'>Name</label>
                         <input
                             className='input is-expanded'
                             value={name}
                             onChange={handleNameChange}
+                        />
+                    </div>
+
+                    <div className='field'>
+                        <label className='label'>Cost</label>
+                        <input
+                            className='input is-expanded'
+                            value={cost || ''}
+                            onChange={handleCostChange}
+                            type='number'
                         />
                     </div>
                 </div>
